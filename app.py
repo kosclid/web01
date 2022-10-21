1ab0b6020377b198c6bc948b313c0c1b00d3b267
kosclid <kos-200@hanmail.net>
2022-10-21 17:10:06 +0900

    2022_10_21정리

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import os
+import dbconn as db
 
 app = Flask(__name__)
 
@@ -7,14 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
+
+
 
 @app.route('/test/<username>')
 def test(username):
     print(username)
     return render_template('test_result.html', name = username)
+
+
 
 @app.route('/methodin')
 def methodin():
@@ -33,6 +40,8 @@ def methodout():
 
 # get으로 들어올때도 받고 post로 들어올 때도 받겠다는 의미 기본값은 GET이다.->걍 /해서 호출하는거
 
+
+
 @app.route('/fileupload', methods = ['GET', 'POST'])
 def fileupload():
     if request.method =='GET':
@@ -45,6 +54,59 @@ def fileupload():
 
         return redirect('/')
 
+
+
+@app.route('/bloglist')
+def bloglist():
+    conn = db.dbconn()
+    cursor = conn.cursor()
+    sql = ''' select * from blog '''
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    print(rows)
+    return render_template('bloglist.html', data = rows)
+
+@app.route('/blogform', methods = ['GET', 'POST'])
+def blogform():
+    if request.method == 'GET':
+        return render_template('blogform.html')
+    else:
+        f = request.files['formFile']
+        path = os.path.dirname(__file__)+'/static/blog/img/'+f.filename
+        print(path)
+        f.save(path)
+        print('저장성공')
+        print(request.form)
+        conn = db.dbconn()
+        cursor = conn.cursor()
+        sql = '''insert into blog (title, content, img_path) values(?,?,?)'''
+        data = [request.form['title'], request.form['content'], '/static/blog/img/'+f.filename]
+        cursor.execute(sql, data)
+        conn.commit()
+        conn.close()
+        # 글을 db에 저장
+        return redirect('/bloglist')
+
+@app.route('/blog/<int:id>')
+def blogcontent(id):
+    conn = db.dbconn()
+    cursor = conn.cursor()
+    sql = ''' select * from blog where id=? '''
+    cursor.execute(sql, id)
+    rows = cursor.fetchone()
+    conn.commit()
+    conn.close()
+    return render_template('blog_content.html', data = rows)
+
+@app.route('/blogdelete/<int:id>')
+def blogdelete(id):
+    conn = db.dbconn()
+    cursor = conn.cursor()
+    sql = ''' delete blog where id=? '''
+    cursor.execute(sql, id)
+    conn.commit()
+    conn.close()
+    return redirect('/bloglist')
 
 if __name__=='__main__':
     app.run(debug = True, port = 80)
